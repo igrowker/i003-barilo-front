a79cce6ff48c2e1cef1ac6f20f467a9c3c85454b
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-09-20 20:35:50 -0300

    implementación i18n, cambio de idioma

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
-function Home() {
-  return (
-    <div>Home</div>
-  )
-}
+import { useTranslation } from 'react-i18next';
 
-export default Home
+const Home: React.FC = () => {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <div className='text-black'>
+      <h1>{t('welcome')}</h1>
+      <p>{t('description')}</p>
+      <button onClick={() => changeLanguage('es')}>Español</button>
+      <button onClick={() => changeLanguage('en')}>English</button>
+    </div>
+  );
+};
+
+export default Home;
