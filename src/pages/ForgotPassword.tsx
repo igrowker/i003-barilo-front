bc93183053bd2643edd94e26125940c43afce17e
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-10-10 15:56:49 -0300

    Password

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,17 +1,19 @@
-import { LoginFormComponent } from "@/components/LoginFormComponent";
+import { ForgotPasswordComponent } from "@/components/ForgotPasswordComponent";
 import logo from "@/assets/images/imago.webp";
 
-export default function Login() {
+export default function ForgotPassword() {
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen px-4 bg-white py-14 md:flex md:justify-center md:items-center">
+        {" "}
         <img src={logo} alt="Barilo" className="w-[250px] h-auto" />
         <section className="flex flex-col justify-center w-full h-auto max-w-[366px] px-4">
           <div className="space-y-4">
             <span className="self-stretch text-[#006BA8] text-[14.92px] font-normal font-secondary leading-[17.91px]">
-              Inicia sesión para acceder a tu cuenta
+              Ingresa tu correo para que enviemos un enlace para recuperar tu
+              contraseña
             </span>
-            <LoginFormComponent />
+            <ForgotPasswordComponent />
           </div>
         </section>
       </div>
