48076df4fb50ec48ad9d60d2891f970296dbd8be
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-10-13 21:47:53 -0300

    Correcciones varias

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,45 @@
-import { CiSettings } from "react-icons/ci"
-import { FaRegBell } from "react-icons/fa"
-import ProfilePicture from '@/assets/images/excursionCard.webp'
-import { Link } from "react-router-dom"
-import { useUserProfile } from "@/hooks/useUserProfile"
-import { useTranslation } from "react-i18next"
-
+import { CiSettings } from "react-icons/ci";
+import { FaRegBell } from "react-icons/fa";
+import ProfilePicture from "@/assets/images/excursionCard.webp";
+import { Link } from "react-router-dom";
+import { useUserProfile } from "@/hooks/useUserProfile";
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
-    const { t } = useTranslation()
-    const { profile } = useUserProfile();
-    
+  const { t } = useTranslation();
+  const { profile } = useUserProfile();
+
   return (
     <div className="flex justify-between px-8 pt-4 pb-3">
-        <div className="flex items-center gap-1">
-            <Link to="/profile">
-                <img src={ProfilePicture} alt="profile picture" className="w-12 h-12 rounded-full cursor-pointer"/>
-            </Link>
-
-
-                <div className="leading-tight">
-                <p className="text-[--secondary-celeste]">{t('home.header')}</p>
-                <p className="text-[--primary-celeste] font-bold">{profile ? profile.name : 'Cargando...'}</p>
-            </div>
-
+      <div className="flex items-center gap-1">
+        <Link to="/profile">
+          <img
+            src={ProfilePicture}
+            alt="profile picture"
+            className="w-12 h-12 rounded-full cursor-pointer"
+          />
+        </Link>
+        <div className="leading-tight">
+          <p className="text-[--secondary-celeste]">{t("home.header")}</p>
+          <p className="text-[--primary-celeste] font-bold">
+            {profile ? profile.name : t("loading")}
+          </p>
         </div>
-        <div className="flex items-center gap-1">
-            <Link to="/settings">
-                <CiSettings size={35} className="bg-[--inactive-button-bg] rounded-full p-1 text-[--primary-celeste] "/>
-            </Link>
-            <FaRegBell size={32} className="bg-[--inactive-button-bg] rounded-full p-1.5 text-[--primary-celeste]"/>
-        </div>
+      </div>
+      <div className="flex items-center gap-1">
+        <Link to="/profileSettings">
+          <CiSettings
+            size={35}
+            className="bg-[--inactive-button-bg] rounded-full p-1 text-[--primary-celeste] "
+          />
+        </Link>
+        <FaRegBell
+          size={32}
+          className="bg-[--inactive-button-bg] rounded-full p-1.5 text-[--primary-celeste]"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
