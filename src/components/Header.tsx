17f62f2f71b1493ad294d90ca9625e0b7bfd2ef8
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-10-14 01:53:17 -0300

    components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,36 +10,40 @@ const Header = () => {
   const { profile } = useUserProfile();
 
   return (
-    <div className="flex justify-between px-8 pt-4 pb-3">
-      <div className="flex items-center gap-1">
-        <Link to="/profile">
-          <img
-            src={ProfilePicture}
-            alt="profile picture"
-            className="w-12 h-12 rounded-full cursor-pointer"
-          />
-        </Link>
-        <div className="leading-tight">
-          <p className="text-[--secondary-celeste]">{t("home.header")}</p>
-          <p className="text-[--primary-celeste] font-bold">
-            {profile ? profile.name : t("loading")}
-          </p>
+    <div className="sticky top-0 left-0 z-10 w-full bg-white shadow-md">
+      <div className="flex justify-between px-8 pt-4 pb-3">
+        <div className="flex items-center gap-1">
+          <Link to="/profile">
+            <img
+              src={ProfilePicture}
+              alt="profile picture"
+              className="w-12 h-12 rounded-full cursor-pointer"
+            />
+          </Link>
+          <div className="leading-tight">
+            <p className="text-[--secondary-celeste]">{t("home.header")}</p>
+            <p className="text-[--primary-celeste] font-bold">
+              {profile ? profile.name : t("loading")}
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="flex items-center gap-1">
-        <Link to="/profileSettings">
-          <CiSettings
-            size={35}
-            className="bg-[--inactive-button-bg] rounded-full p-1 text-[--primary-celeste] "
+        <div className="flex items-center gap-1">
+          <Link to="/profileSettings">
+            <CiSettings
+              size={35}
+              className="bg-[--inactive-button-bg] rounded-full p-1 text-[--primary-celeste]"
+            />
+          </Link>
+          <FaRegBell
+            size={32}
+            className="bg-[--inactive-button-bg] rounded-full p-1.5 text-[--primary-celeste]"
           />
-        </Link>
-        <FaRegBell
-          size={32}
-          className="bg-[--inactive-button-bg] rounded-full p-1.5 text-[--primary-celeste]"
-        />
+        </div>
       </div>
     </div>
   );
 };
 
 export default Header;
+
+
